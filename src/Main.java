9f6c0446de320c0f36ce71b6bb0af068fc153a27
Shiao <roberto.shiao@epn.edu.ec>
2022-11-20 17:36:40 -0500

    Se añadio el objeto vehiculos

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        vehiculos vh = new vehiculos();
+        int menu, opcion;
+            do {
+                vehiculos.mostrarMenu(vh);
+                System.out.print("Seleccione una de las opciones del menu: ");
+                menu = sc.nextInt();
+
+                switch (menu) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+
+                    default:
+                        System.out.println("Opcion no disponible!!!!!");
+                        break;
+                }
+                System.out.println("Desea continuar con el programa 1.SI");
+                opcion = sc.nextInt();
+            } while (opcion == 1);
+    }
+
 }
+
