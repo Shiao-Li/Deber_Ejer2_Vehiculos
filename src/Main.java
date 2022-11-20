e615a208c7c7b1a5b2400e9cd159440155b1047f
Shiao <roberto.shiao@epn.edu.ec>
2022-11-20 18:19:44 -0500

    Pide datos del auto 1

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,32 @@ public class Main {
         vehiculos vh = new vehiculos();
         int menu, opcion;
             do {
-                vehiculos.mostrarMenu(vh);
+                vh.mostrarMenu(vh);
                 System.out.print("Seleccione una de las opciones del menu: ");
                 menu = sc.nextInt();
 
                 switch (menu) {
                     case 1:
-
+                        System.out.println("Datos del auto 1");
+                        String color1, tipo1, modelo1, placa1, marca1;
+                        System.out.println("Ingrese la marca del auto: ");
+                        marca1 = sc.next();
+                        vh.setMarca(marca1);
+                        System.out.println("Ingrese el modelo del auto: ");
+                        modelo1 = sc.next();
+                        vh.setModelo(modelo1);
+                        System.out.println("Ingrese el tipo del auto: ");
+                        tipo1 = sc.next();
+                        vh.setTipo(tipo1);
+                        System.out.println("Ingrese el Color del auto: ");
+                        color1 = sc.next();
+                        vh.setColor(color1);
+                        System.out.println("Ingrese el placa del auto: ");
+                        placa1 = sc.next();
+                        vh.setPlaca(placa1);
                         break;
                     case 2:
-
+                        vh.mostrarDatos(vh);
                         break;
 
                     default:
