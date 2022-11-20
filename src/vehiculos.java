3aa6b7b0ff7f10691adf83980afabcd09f6c25ca
Shiao <roberto.shiao@epn.edu.ec>
2022-11-20 17:35:46 -0500

    Se añadio el metodo del menu

--- src/vehiculos.java
+++ src/vehiculos.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Vehiculos {
+public class vehiculos {
+    String color, tipo, modelo, placa, Marca;
+
+    public vehiculos(){
+
+    }
+    public static void mostrarMenu(vehiculos vh) {
+        System.out.println("****MENU****");
+        System.out.println("1. Ingresar datos ");
+        System.out.println("2. Mostrar datos");
+    }
 }
